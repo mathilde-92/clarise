e1a7dc44d8968fdab2154aa416b0db671cbac38f
mathilde-92 <mir.lehalle@gmail.com>
2026-06-18 19:25:23 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ClariseApp from "./ClarisePrototype.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ClariseApp />
+  </React.StrictMode>
+);
 
